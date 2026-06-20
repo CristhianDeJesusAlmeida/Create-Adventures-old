df5898a95cd8234caf2e0db485b61e9021552645
Correntefantasma <gatinho_sky432@hotmail.com>
2026-06-19 21:41:43 -0300

    "chore: otimiza gitignore para liberar ftbquests e scripts"

---
+++ kubejs/server_scripts/loot_balance.js
@@ -0,0 +1,12 @@
+LootJS.modifiers((event) => {
+    // Alvo: Todas as tabelas de loot do mod de estruturas do Create
+    event.addLootTableModifier(/create_structures:.*/)
+        // Substitui itens avançados por equivalentes básicos de forma justa
+        .replaceStack('create:brass_casing', 'create:andesite_casing')
+        .replaceStack('create:precision_mechanism', 'create:electron_tube')
+        .replaceStack('create:brass_ingot', 'create:zinc_ingot')
+        
+        // Reduz a quantidade de metais valiosos pela metade para balancear
+        .modifyLoot('minecraft:iron_ingot', (stack) => stack.withCount(Math.max(1, Math.floor(stack.getCount() / 2))))
+        .modifyLoot('minecraft:gold_ingot', (stack) => stack.withCount(Math.max(1, Math.floor(stack.getCount() / 2))));
+});
