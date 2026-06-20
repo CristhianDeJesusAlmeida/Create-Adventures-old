8ff82b23521fbab0695febe858990005ddea25b1
Correntefantasma <gatinho_sky432@hotmail.com>
2026-06-20 17:58:46 -0300

    att

--- kubejs/server_scripts/loot_balance.js
+++ kubejs/server_scripts/loot_balance.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 // CORREÇÃO: "lootjs" em minúsculo conforme a API oficial estável da 1.21.1
 lootjs.modifiers((event) => {
-=======
-// Gatilho oficial global para carregar os modificadores do LootJS de forma estável
-LootJS.modifiers((event) => {
->>>>>>> 0ee5cb383828c1693d0b92b14bb770dba7890b38
 
     // Alvo: Todas as tabelas de loot do mod de estruturas do Create
     event.addLootTableModifier(/create_structures:.*/)
